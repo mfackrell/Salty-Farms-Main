d90f0b8222bdfdb49329b9251bf3f48714a0f05a
mfackrell <mfackrell@contract-cfo.com>
2026-03-17 10:32:14 -0500

    Change server to listen on all network interfaces

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -2,7 +2,6 @@ import { createApp } from './app.js';
 import { env } from './config/env.js';
 
 const app = createApp();
-app.listen(env.PORT, () => {
-  // eslint-disable-next-line no-console
+app.listen(env.PORT, '0.0.0.0', () => {
   console.log(`API listening on ${env.PORT}`);
-});
+});;
